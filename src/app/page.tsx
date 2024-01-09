61a120debd34ff132a6528e09e4e3a9731c49f0e
Vailery <leraliabik8@gmail.com>
2024-01-09 18:57:53 +0300

    feat: add basic layout via chakra-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,68 @@
+"use client";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  LinkBox,
+  Button,
+  Divider,
+  Stack,
+  Text,
+  IconButton,
+  Input,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Badge,
+} from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+
 export default function Home() {
-  return <></>;
+  return (
+    <Flex flexDirection="column" w="100%" p="10">
+      <Box w="100%">
+        <Heading p="4">SQL Builder</Heading>
+      </Box>
+
+      <LinkBox p="5" borderWidth="1px" w="auto" rounded="md">
+        <Box>Query</Box>
+        <Stack direction="row" h="100%" maxH="100%" p={4}>
+          <Divider orientation="vertical" h="auto" borderWidth="2px" />
+
+          <Stack direction="column">
+            <Text>SELECT</Text>
+
+            <Input placeholder="Enter the columns names" />
+
+            <Text>FROM</Text>
+
+            <Input placeholder="Enter the table name" />
+            <Box>
+              <Menu>
+                <MenuButton
+                  as={IconButton}
+                  aria-label="Options"
+                  icon={<AddIcon />}
+                  variant="outline"
+                  w="fit-content"
+                  size="sm"
+                />
+                <MenuList>
+                  <MenuItem icon={<span>&</span>}>AND</MenuItem>
+                  <MenuItem icon={<span>|</span>}>OR</MenuItem>
+                </MenuList>
+              </Menu>
+              <Badge ml={3}>Optional</Badge>
+            </Box>
+          </Stack>
+        </Stack>
+
+        <Button colorScheme="teal" variant="outline" isDisabled={true}>
+          Send Query
+        </Button>
+      </LinkBox>
+    </Flex>
+  );
 }
